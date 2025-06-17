3381567b3f6e17f282ff70542651c195b87cabf2
Syed Muhammad Ali Zagham <syedalizagham.student@gmail.com>
2025-06-16 21:22:31 -0500

    add code for recursiveInsert and recursiveContains methods

--- BST/BinarySearchTree.java
+++ BST/BinarySearchTree.java
@@ -52,4 +52,47 @@ public class BinarySearchTree {
         return false;
 
      }
+     
+     // recursive insert method 
+     private Node recursiveInsert(Node currentNode, int value) {
+        if (currentNode == null) { // if the current node is null
+            return new Node(value);  // create a new node
+        } else if (value < currentNode.value){ // if the value is less than the current node
+            currentNode.left = recursiveInsert(currentNode.left, value); // assign the left child
+        } else {
+            currentNode.right = recursiveInsert(currentNode.right, value); // assign the right child
+        }
+ 
+        return currentNode; // return the current node
+     }
+     
+     // overloaded recursive insert method 
+     public void recursiveInsert (int value) {
+        root = recursiveInsert(root, value); // assign the root
+        
+     }
+     
+     // recursive contains method
+     public Boolean recursiveContains(Node currentNode, int value) {
+        if (currentNode == null) { // if the current node is null
+            return false; // return false
+        } 
+
+        if (currentNode.value == value) {  // if the value is equal to the current node
+            return true;  // return true
+        } else if (value < currentNode.value) {  // if the value is less than the current node
+            return recursiveContains(currentNode.left, value); // return the left child
+        } else {
+            return recursiveContains(currentNode.right, value);  // return the right child
+        }
+     }
+
+     // Overloaded public version that starts from root
+     public Boolean recursiveContains(int value) {
+        return recursiveContains(root, value);
+
+       }
+
+
+     
 }
