ae540b702dc40258e0651566714f8aec18768369
Syed Muhammad Ali Zagham <syedalizagham.student@gmail.com>
2025-08-03 23:41:15 -0500

    add readme file

--- BitsManipulation/NumOfBits/Solution.java
+++ BitsManipulation/NumOfBits/Solution.java
@@ -4,7 +4,7 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         while (n != 0) {
-            count += (n & 1);
+            count += (n & 1);  // bitwise AND
             n >>>= 1;  // unsigned right shift
         }
         return count;
