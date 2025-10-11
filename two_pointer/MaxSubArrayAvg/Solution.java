1f316f13bb2c159772784f2a63f598cc52135177
Syed Muhammad Ali Zagham <syedalizagham.student@gmail.com>
2025-10-10 23:15:48 -0500

    add code for leetcode 643 with readme

---
+++ two_pointer/MaxSubArrayAvg/Solution.java
@@ -0,0 +1,27 @@
+package two_pointer.MaxSubArrayAvg;
+
+public class Solution {
+    class Solution {
+        public double findMaxAverage(int[] nums, int k) {
+            // Find the sum of the first window (first k elements)
+            double windowSum = 0;
+            for (int i = 0; i < k; i++) {
+                windowSum += nums[i];
+            }
+    
+            // Initialize maxSum with the first window sum
+            double maxSum = windowSum;
+    
+            // Slide the window through the rest of the array
+            for (int i = k; i < nums.length; i++) {
+                windowSum += nums[i] - nums[i - k];  // add new element, remove leftmost
+                maxSum = Math.max(maxSum, windowSum);
+            }
+    
+            // Return maximum average
+            return maxSum / k;
+        }
+    }
+    
+    
+}
