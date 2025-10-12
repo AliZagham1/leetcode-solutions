a11aaa989e916a14a35a59d78fa708a7247a52df
Syed Muhammad Ali Zagham <syedalizagham.student@gmail.com>
2025-10-11 22:15:41 -0500

    add code for leetcode problem number 209 and create a folder for SlidingWindow problems

---
+++ SlidingWindow/MaxSubArrayAvg/Test.java
@@ -0,0 +1,9 @@
+package SlidingWindow.minSubArrayLen.MaxSubArrayAvg;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.findMaxAverage(new int[]{1,12,-5,-6,50,3}, 4)); // Expected: 12.75
+    }
+    
+}
---
+++ SlidingWindow/minSubArrayLen/Test.java
@@ -0,0 +1,11 @@
+package SlidingWindow.minSubArrayLen;
+
+public class Test {
+    int[] nums = {3,0,1,4};
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.minSubArrayLen(4, new int[]{2,3,1,2,4,3})); // Expected: 1
+
+    }
+    
+}
