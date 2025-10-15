a0d8f72c0643e5fd38ae888ebc3ecb5c540cbc2f
Syed Muhammad Ali Zagham <syedalizagham.student@gmail.com>
2025-10-14 20:18:03 -0500

    Add code for leetcode problem number 219 with readme

---
+++ SlidingWindow/containsDuplicateII/Solution.java
@@ -0,0 +1,18 @@
+package SlidingWindow.containsDuplicateII;
+
+public class Solution {
+    public Boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+    
+}
---
+++ SlidingWindow/containsDuplicateII/Test.java
@@ -0,0 +1,11 @@
+package SlidingWindow.containsDuplicateII;
+
+public class Test {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1,2,3,1}, 3)); // Expected: true
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1,0,1,1}, 1)); // Expected: true
+        System.out.println(sol.containsNearbyDuplicate(new int[]{1,2,3,1,2,3}, 2)); // Expected: false
+    }
+    
+}
